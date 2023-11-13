a3b5f859e16991c711a3f0ffe3d78fe2d36dbcd7
Sherzod <qurbonovsherzod2002@gmail.com>
2023-11-13 13:09:03 +0500

    adding all files

--- DifferentTasks/main.go
+++ DifferentTasks/main.go
@@ -7,8 +7,8 @@ func main() {
 	fmt.Scan(&a)
 	var a1, a3 int
 	a1 = a / 100
-	a3 = (a % 100) % 10
-	// fmt.Println(a1, " " , a3)
+	a3 = a % 10
+	fmt.Println(a1, " " , a3)
 	if a1 > a3 {
 		fmt.Println(a1)
 	} else {
