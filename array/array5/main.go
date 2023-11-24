6d28626aff28647c9abc60ee5022bbaea4251381
Sherzod <qurbonovsherzod2002@gmail.com>
2023-11-24 12:05:59 +0500

    fixed

--- array/array5/main.go
+++ array/array5/main.go
@@ -2,41 +2,51 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
-	var n int
-	// var m int
+	var n, min int
+	var f bool
 	fmt.Scan(&n)
-	// k := 1
-	a := make([]int, n+3)
-	b := make([]int, n + 1)
-	// c := make([]int, n)
-	
+	a := make([]int, n + 2)
+
 	for i := 0; i < n; i++ {
 		fmt.Scan(&a[i])
-	}
-	for i := 0; i < n; i++ {
-		if ( a[i] % 2 != 0 ){
-			b[i] = a[i]
+		if (a[i] % 2 != 0 && (a[i] < min || !f) ) {
+			min = a[i]
+			f = true
 		}
 	}
-	sort.Ints(b)
-	for i := 0; i < n; i++ {
 
-			if ( b[i] < b[n] && b[i] != 0 ){
-				fmt.Printf("%v ", b[i])
-			}else{
-				fmt.Println(0)
-				break
-			}
-
+	if f {
+		fmt.Println(min)
+	} else{
+		fmt.Println(0)
 	}
-	
-	
-	// fmt.Println(c)
-	
-	// fmt.Println(m)
-	
 }
+	
+// var n int
+	// // var m int
+	// fmt.Scan(&n)
+	// // k := 1
+	// a := make([]int, n+3)
+	// b := make([]int, n + 1)
+	// // c := make([]int, n)
+	
+	// for i := 0; i < n; i++ {
+	// 	fmt.Scan(&a[i])
+	// }
+	// for i := 0; i < n; i++ {
+	// 	if ( a[i] % 2 != 0 ){
+	// 		b[i] = a[i]
+	// 	}
+	// }
+	// sort.Ints(b)
+	// for i := 0; i < n; i++ {
+
+	// 		if ( b[i] < b[n] && b[i] != 0 ){
+	// 			fmt.Printf("%v ", b[i])
+	// 		}else{
+	// 			fmt.Println(0)
+	// 			break
+	// 		}
